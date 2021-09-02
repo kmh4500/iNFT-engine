1fce3f99eaa42b15369e91a6202455303d284aa4
Ubuntu <ubuntu@ip-172-31-4-145.ap-northeast-1.compute.internal>
2021-09-02 07:07:36 +0000

    add build

--- main.py
+++ main.py
@@ -11,14 +11,15 @@ from sentence_transformers import SentenceTransformer
 from vector_engine.utils import vector_search, id2details
 from transformers import PreTrainedTokenizerFast, GPT2LMHeadModel, GPT2TokenizerFast, GPT2Tokenizer, GPT2Model
 from flask_caching import Cache
+from requests import get
 import os
+import re
 
 os.environ['TORCH_HOME'] = 'models'
 
 lock = threading.Lock() 
 
 config = {
-        "DEBUG": True,          # some Flask specific configs
         "CACHE_TYPE": "SimpleCache",  # Flask-Caching related configs
         "CACHE_DEFAULT_TIMEOUT": 300
         }
@@ -36,7 +37,7 @@ def read_data(data="data/elon_core_memories.csv"):
     lines = []
     for data in ["data/elon_core_memories.txt", "data/elon_clubhouse.txt", "data/lex.txt", "data/joe-rogan.txt"]:
         with open(data) as my_file:
-            lines += my_file.readlines()
+            lines += my_file.read().splitlines()
     return lines
 
 def load_bert_model(name="distilbert-base-nli-stsb-mean-tokens"):
@@ -49,7 +50,7 @@ def load_gpt2_model():
 def load_gpt2_tokenizer():
     return GPT2Tokenizer.from_pretrained('gpt2-xl', cache_dir = "models/")
 
-def load_faiss_index(model, df):
+def load_faiss_index(model, data):
     # Check if GPU is available and use it
     if torch.cuda.is_available():
         model = model.to(torch.device("cuda"))
@@ -70,11 +71,10 @@ def load_faiss_index(model, df):
 
 def generate_text(model, tokenizer, sequence, max_length):
     ids = tokenizer.encode(f'{sequence}', return_tensors='pt').to('cuda')
-    max_length = len(sequence) + 100
     final_outputs = model.generate(
         ids,
         do_sample=True,
-        max_length=max_length,
+        max_length=len(sequence) + 100,
         pad_token_id=model.config.eos_token_id,
         top_k=50,
         top_p=0.95,
@@ -82,17 +82,47 @@ def generate_text(model, tokenizer, sequence, max_length):
         length_penalty=2.0
     )
     result = tokenizer.decode(final_outputs[:, ids.shape[-1]:][0], skip_special_tokens=True)
+    print (result)
     result = result[:result.find("User:")]
     return result.strip()
 
-data = read_data()
+default_data = read_data()
 model = load_bert_model()
-faiss_index = load_faiss_index(model, data)
+default_faiss_index = load_faiss_index(model, default_data)
 gpt2model = load_gpt2_model()
 tokenizer = load_gpt2_tokenizer()
+faiss_dict = {}
+data_dict = {}
 
 print("complete model loading")
 
+@app.route('/build/<channel>', methods=['POST'])
+def build(channel):
+    data = request.form.get('data')
+    print (channel)
+    print (data)
+    if not channel or not data:
+        return 'please provide channel and data', 200
+    data_list = data.split(',')
+    lines = []
+    for url in data_list:
+        response = get(url)
+        # ignore too big file, invalid files
+        if response and response.content and len(response.content) < 1000000:
+            lines += response.content.decode('utf-8').splitlines()
+    print (lines)
+    if lines:
+        faiss_dict[channel] = load_faiss_index(model, lines)
+        data_dict[channel] = lines
+        cache.clear()
+        return 'done:', 200
+    else:
+        return 'invalid data', 500
+
+@app.route('/has_channel', methods=['GET'])
+def has_channel():
+    channel = request.args.get('channel', default = "", type = str)
+    return {has_channel: channel in faiss_dict}, 200
 
 ##
 # Get post request page.
@@ -101,38 +131,51 @@ print("complete model loading")
 def generate():
     context = request.args.get('context', default = "", type = str)
     user_input = request.args.get('message', default = "", type = str)
-
-    num_results = 10
+    channel = request.args.get('channel', default = "", type = str)
+    name = request.args.get('name', default = "Elon", type = str)
+    faiss_index = default_faiss_index
+    data = default_data
+    if channel and channel in faiss_dict:
+        faiss_index = faiss_dict[channel]
+        data = data_dict[channel]
+    else:
+        return 'invalid channel', 500
+    num_results = 3
     # Fetch results
     if user_input:
         # Get paper IDs
         D, I = vector_search([user_input], model, faiss_index, num_results)
-        # memories = [data[i] for i in I[0]]
-        print (I[0])
         memories = []
         for i in I[0]:
-            if i + 1 < len(data) and data[i].find("User") > 0:
+            if i + 1 < len(data) and data[i].find("User") >= 0:
+                memories.append(data[i])
                 memories.append(data[i + 1])
             else:
                 memories.append(data[i])
         # Get individual results
         print (memories)
-        prompt = ""
+        prompt = (
+                f"The following is a conversation between User and {name}\n"
+                f"{context}\n"
+                )
         for memory in memories:
-            prompt += memory[0] + '\n'
-        prompt += """
-The following is a conversation between User and Elon.
-""" + context + """
-
-User: What is your name?
-Elon: My name is Elon Musk.
-User: """ + user_input + "\nElon:"
+            if len(memory) > 100:
+                memory = memory[:100]
+            prompt += memory + '\n'
+        prompt += (
+                f"User: What is your name?\n"
+                f"{name}: My name is {name}\n"
+                f"User: {user_input}\n"
+                f"{name}:"
+                )
+        print (prompt)
         if lock.locked():
-            results = memories[0].replace("User:", "").replace("Elon:", "").strip() 
+            results = memories[0].replace("User:", "").replace(f"{name}:", "").strip() 
             print (results)
             return results
         lock.acquire()
-        results = generate_text(gpt2model, tokenizer, prompt, 500)
+        results = generate_text(gpt2model, tokenizer, prompt, 400)
+        results = results.replace(f"{name}:", "")
         lock.release()
         print (results)
         return results, 200
