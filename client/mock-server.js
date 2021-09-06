b1e01c0779b6e647f3f0d048fea8f7a8323a667f
kmh4500 <kmh4500@kmh4500ui-iMac.local>
2021-09-06 17:05:30 +0900

    dockerfile

--- client/mock-server.js
+++ client/mock-server.js
@@ -1,6 +1,8 @@
 var http = require('http');
 const url = require('url');
 
+console.log("mock server started..")
+
 http.createServer(function (req, res) {
   const parsedUrl = url.parse(req.url, true);
 
---
+++ client/pages/api/engine.js
@@ -0,0 +1,13 @@
+const axios = require('axios');
+
+export default async function handler(req, res) {
+  let channel = req.query.channel;
+  let context = req.query.context;
+  let message = req.query.message;
+  console.log(channel);
+  console.log(context);
+  console.log(message);
+  let result = await axios.get(`http://localhost:5000/chat?channel=${channel}&context=${context}&message=${message}`)
+  console.log(result.data)
+  res.status(200).send(result.data)
+}
